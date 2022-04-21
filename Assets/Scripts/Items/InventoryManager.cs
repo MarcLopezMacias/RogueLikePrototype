0bc477bb35583f7acb160da7f6c0df2e0deac428
marc.lopezma.7e3@itb.cat <marc.lopezma.7e3@itb.cat>
2022-04-21 15:03:44 +0200

    Finished Inventory but needs FIX

--- Assets/Scripts/Items/InventoryManager.cs
+++ Assets/Scripts/Items/InventoryManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public static int InventorySize = 5;
+
+    public GameObject slotPrefab;
+    public List<InventorySlot> inventorySlots = new List<InventorySlot>(InventorySize);
+
+    private void OnEnable()
     {
-        
+        Inventory.OnInventoryChange += DrawInventory;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Inventory.OnInventoryChange -= DrawInventory;
+    }
+
+    void ResetInventory()
+    {
+        foreach(Transform childTransform in transform)
+        {
+            Destroy(childTransform.gameObject);
+        }
+
+        inventorySlots = new List<InventorySlot>(InventorySize);
+    }
+
+    void DrawInventory(List<InventoryItem> inventory)
+    {
+        ResetInventory();
+
+        for(int i = 0; i <  inventorySlots.Capacity; i++)
+        {
+            CreateInventorySlot();
+        }
+
+        for(int i = 0; i < inventory.Count; i++)
+        {
+            inventorySlots[i].DrawSlot(inventory[i]);
+        }
+    }
+
+    void CreateInventorySlot()
+    {
+        GameObject newSlot = Instantiate(slotPrefab);
+        newSlot.transform.SetParent(transform, false);
+
+        InventorySlot newSlotComponent = newSlot.GetComponent<InventorySlot>();
+        newSlotComponent.ClearSlot();
+
+        inventorySlots.Add(newSlotComponent);
     }
 }
---
+++ Assets/Scripts/Items/InventorySlot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+
+    public Image icon;
+    public Text labelText;
+    public Text stackSizeText;
+
+    public void ClearSlot()
+    {
+        icon.enabled = false;
+        labelText.enabled = false;
+        stackSizeText.enabled = false;
+    }
+
+    public void DrawSlot(InventoryItem item)
+    {
+        if (item == null)
+        {
+            ClearSlot();
+            return;
+        }
+
+        icon.enabled = true;
+        labelText.enabled = true;
+        stackSizeText.enabled = true;
+
+        icon.sprite = item.itemData.Image;
+        labelText.text = item.itemData.Name;
+        stackSizeText.text = item.stackSize.ToString();
+
+    }
+
+
+}
